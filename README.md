d57b700678473256878f12f090fb24474d48e4d2
Mahxzx <st20260582@outlook.cardiffmet.ac.uk>
2023-12-21 15:36:39 +0530

    Updated dashboard manage accounts

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
 # outer-clove-
+
+1.	Index.html   <-Landing Page 
+2.	Menu.html  <-Menu page 
+3.	Branches.html <-branch page 
+4.	Login.html <- login page (user type selection input -> admin, customer)
+
+5.	Admindashboard.html <- andmin dashboard
+a.	UpdatePromotions.html 
+b.	ManageProfile.html 
+c.	AccountsManagement.html 
+d.	ManageReservations.html 
+
+6.	Customerdashboard.html <- customer dashboard pge
+a.	Reservation.html (using a form)
+b.	ManageProfile.html 
+ 
+Html sections
+Index.html 
+	Header with navigation 	
+	Overview
+	 Promotions 	
+	Feedback 
+	Contact
+	Footer 
+
+Menu.html
+header with navigation
+menu list 	
+contact 
+footer 
+
+branches.html
+	header with navigation
+	location list 	
+	contact 
+	footer
+
+login.html
+	header with navigation
+	login form 
+	contact 
+	footer
+
+	
+AdminDashboard.html
+	header with navigation
+	welcome section 
+	 admin function section with link pages 
+	contact 	
+	footer
+updatePromotion.html
+	header with navigation 
+	
+		section1 -> display the current promotins
+			a table with column of pid,ptitle,pdescriptiion,plink,pimage. for the each row end give two buttons such as update and delete. The user canm only change the ptitle,pdescriptiion,plink,pimage but cant change pid so just display but disabled. 
+		Section2 -> add prmotion 
+		 A from to ask for the promotion details pid,ptitle,pdescriptiion,plink,pimage.with add button once the user insert the data display in the above section table
+	contact	
+	footer
+
+ManageProfile.html
+	header with navigation 
+	form to update profile
+	contact	
+	footer
+
+AccountsManagement.html 
+header with navigation 
+	form to add,delete and update user accounts 
+	contact	
+	footer
+
+ManageReservations.html 
+header with navigation 
+list of reservations 
+	form to add, delete update reservations 
+	contact	
+	footer
+
+Customerdashboard.html
+	header with navigation
+	welcome section 
+	 Customer function section with link pages 
+	contact 	
+	footer
+
+Reservation.html 
+header with navigation 
+	list of reservations 
+	form to add reservations 
+	contact	
+	footer
+
+ManageProfile.html 
+	header with navigation 
+	form to update profile
+	contact	
+	footer
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -36,3 +36,96 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error('Error fetching data:', error));
 });
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch data from JSON file
+    fetch('/json/datas.json')
+        .then(response => response.json())
+        .then(data => {
+            const promotionTableBody = document.getElementById('promotionTableBody');
+
+            // Display existing promotions in the table
+            data.promotions.forEach(promotion => {
+                addPromotionToTable(promotionTableBody, promotion);
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
+
+function addPromotionToTable(tableBody, promotion) {
+    const row = tableBody.insertRow();
+
+    const pidCell = row.insertCell(0);
+    pidCell.textContent = promotion.pid;
+    pidCell.setAttribute('contenteditable', 'true');
+
+    const titleCell = row.insertCell(1);
+    titleCell.textContent = promotion.ptitle;
+    titleCell.setAttribute('contenteditable', 'true');
+
+    const descriptionCell = row.insertCell(2);
+    descriptionCell.textContent = promotion.pdescription;
+    descriptionCell.setAttribute('contenteditable', 'true');
+
+    const linkCell = row.insertCell(3);
+    linkCell.textContent = promotion.plink;
+    linkCell.setAttribute('contenteditable', 'true');
+
+    const imageCell = row.insertCell(4);
+    imageCell.innerHTML = `<img src="${promotion.pimage}" alt="Promotion Image">`;
+
+    const actionCell = row.insertCell(5);
+    actionCell.innerHTML = `<button onclick="updatePromotion('${promotion.pid}')">Update</button>
+                           <button onclick="deletePromotion('${promotion.pid}')">Delete</button>`;
+}
+
+function updatePromotion(pid) {
+    // Implement update logic based on your requirements
+    alert(`Update promotion with ID: ${pid}`);
+}
+
+function deletePromotion(pid) {
+    // Implement delete logic based on your requirements
+    alert(`Delete promotion with ID: ${pid}`);
+}
+function addPromotion() {
+    // Get input values
+    const pid = document.getElementById('pid').value;
+    const ptitle = document.getElementById('ptitle').value;
+    const pdescription = document.getElementById('pdescription').value;
+    const plink = document.getElementById('plink').value;
+    const pimage = document.getElementById('pimage').value;
+
+    // Check if any field is empty
+    if (!ptitle || !pdescription || !plink || !pimage) {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    // Create a new promotion object
+    const newPromotion = {
+        pid,
+        ptitle,
+        pdescription,
+        plink,
+        pimage
+    };
+
+    // Add the new promotion to the table and clear the form
+    const promotionTableBody = document.getElementById('promotionTableBody');
+    addPromotionToTable(promotionTableBody, newPromotion);
+
+    // Clear form fields
+    document.getElementById('ptitle').value = '';
+    document.getElementById('pdescription').value = '';
+    document.getElementById('plink').value = '';
+    document.getElementById('pimage').value = '';
+
+    // Generate a new PID (you may implement this based on your needs)
+    const newPid = generateNewPid();
+    document.getElementById('pid').value = newPid;
+}
+
+function generateNewPid() {
+    // Implement your logic to generate a new PID (e.g., get the current maximum PID and increment)
+    return 'NewPID';
+}
